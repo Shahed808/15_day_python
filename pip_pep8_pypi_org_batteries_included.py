980824729944486fdf945a2e0b58177c96ef1d37
Shahed808 <107167352+Shahed808@users.noreply.github.com>
2022-07-05 11:23:24 +0530

    Add files via upload

---
+++ pip_pep8_pypi_org_batteries_included.py
@@ -0,0 +1,20 @@
+'''
+PIP - Python installation package, PIP  is the package manager for python.
+PEP8 - Python enhance proposal, It's coding style convention
+pypi - Python package index
+pypi.org - Website for all package available in python
+batteries_included - You will find all standard packages in this webste(https://pythonanywhere.com/batteres_included/)
+chocolatey is the package manager for windows
+Homebrew is the package manager for Mac and Linux
+
+'''
+import emoji
+
+print(emoji.emojize(':grinning_face_with_big_eyes:'))
+print(emoji.emojize(':winking_face_with_tongue:'))
+
+print(emoji.demojize('😒'))
+
+
+
+
